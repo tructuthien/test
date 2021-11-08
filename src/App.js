41252bcd99005f3258795b0cd4d22d764e21174d
tructuthien <binhpvsteam3@gmail.com>
2021-11-08 23:32:12 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-import {
-  BrowserRouter as Router, Route, Switch
-} from "react-router-dom";
 import './App.css';
-import Footer from "./components/Footer/Footer";
-import Navigation from "./components/Navbar/Navigation";
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
-import Home from './pages/Home/Home';
-import NotFound from "./pages/NotFound/NotFound";
-import PostDetail from "./pages/PostDetail/PostDetail";
-import Posts from './pages/Posts/Posts';
 
 
 
@@ -25,43 +14,6 @@ const App = () => {
 
     <div>
       <h1>test</h1>
-      <Navigation />
-
-      <Router>
-        <Switch>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route exact path="/posts">
-            <Posts />
-          </Route>
-
-          <Route path="/posts/:id">
-            <PostDetail />
-          </Route>
-
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          {/* <Route component={NotFound} /> */}
-
-          <Route path="*" >
-            <NotFound />
-          </Route>
-
-        </Switch>
-      </Router>
-
-      <hr />
-
-      <Footer />
 
     </div>
 
--- src/common/constant.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "https://jsonapiplaceholder"
-
-
-export const API_URL = {
-
-    GET_POSTS: "/post/"
-    
-}
